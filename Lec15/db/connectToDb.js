df8b6b0341df5fabbba18a264dec661ee006038f
Datodia <dato.diasamidze.02@gmail.com>
2025-05-23 22:21:41 +0400

    MONGODB in express

---
+++ Lec15/db/connectToDb.js
@@ -0,0 +1,13 @@
+const { default: mongoose } = require('mongoose')
+
+require('dotenv').config()
+
+module.exports = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected Successfully")
+    }catch(e){
+        console.log("CouldNot connect to DB")
+    }
+}
+
